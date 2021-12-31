eb91667a184f518fcbf4ff98a077e0c0da1cc2ba
okshi <johnstarhafiz@gmail.com>
2022-01-01 00:53:50 +0600

    Hafizur Done Next One

---
+++ src/context/AuthProvidert.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuthProvidert = () => {
+     return (
+          <div>
+               <h1>Hello</h1>
+          </div>
+     );
+};
+
+export default AuthProvidert;

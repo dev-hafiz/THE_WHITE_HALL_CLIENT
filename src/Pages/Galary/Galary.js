ade63769a898e6f83bf1a1dc45356b376046c3b1
Mehedi Hasan Saikat <29018897+mehedi-hasan-saikat@users.noreply.github.com>
2022-01-02 02:23:03 +0600

    Updated

--- src/Pages/Galary/Galary.js
+++ src/Pages/Galary/Galary.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Galary.css';
+
 const Galary = () => {
     return (
         <div>
-           <h1>This is Galary page</h1> 
+            
         </div>
     );
 };

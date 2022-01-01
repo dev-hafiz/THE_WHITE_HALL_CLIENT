af49467500279ffae534a4115c3873ffe80f1200
Mehedi Hasan Saikat <29018897+mehedi-hasan-saikat@users.noreply.github.com>
2022-01-01 12:41:46 +0600

    Carousel Update

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,14 +1,11 @@
 import React from 'react';
+
+
 import Banner from './Banner/Banner';
 const Home = () => {
     return (
-<<<<<<< HEAD
 <div>
 <Banner></Banner>
-=======
-        <div>
-
->>>>>>> ba6f70b078f655946c7e7dc2e2a4fe2777412370
 
         </div>
     );

4b64be947b8e3a92548b6a7bb329a7819b0a9394
Mehedi Hasan Saikat <29018897+mehedi-hasan-saikat@users.noreply.github.com>
2022-01-01 12:32:02 +0600

    Bannaer Add

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Header from '../Shared/Header/Header';
+import Banner from './Banner/Banner';
 const Home = () => {
     return (
 <div>
-
+<Banner></Banner>
 
 </div>
    
